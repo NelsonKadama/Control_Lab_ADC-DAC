c10c4e2d5329404cfa8e498b8600cad4ab560753
Nelson Kadama <sskadama@gmail.com>
2017-02-20 16:12:13 +0200

    Decided to create new form instead

---
+++ DAQ.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class DAQ : Form
+    {
+        private string input;
+        private double input_voltage;
+        public DAQ()
+        {
+            InitializeComponent();
+        }
+
+        public void PortOpen()
+        {
+            serialPort1.Open();
+        }
+
+        private void getData(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            input = serialPort1.ReadLine();
+            this.Invoke(new EventHandler(displaydata_event));
+        }
+
+        private void displaydata_event(object sender, EventArgs e)
+        {
+
+            textBox1.Text = input;
+
+        }
+        
+
+        public void PortClose()
+        {
+            serialPort1.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PortOpen();
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -27,44 +27,49 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.BackColor = Color.Brown;
-            label1.Text = "HelloWorld!"; // take out
+            //this.BackColor = Color.Brown;
+            //label1.Text = "HelloWorld!"; // take out
 
-            myport = new SerialPort();
-            myport.BaudRate = 9600;
-            myport.PortName = "COM9";
-            myport.Parity = Parity.None;
-            myport.DataBits = 8;
-            myport.StopBits = StopBits.One;
-            myport.DataReceived += new SerialDataReceivedEventHandler(myport_DataReceived);
-            
-            try
-            {
-                myport.Open();
-            }
+            //myport = new SerialPort();
+            //myport.BaudRate = 9600;
+            //myport.PortName = "COM9";
+            //myport.Parity = Parity.None;
+            //myport.DataBits = 8;
+            //myport.StopBits = StopBits.One;
+            //myport.DataReceived += new SerialDataReceivedEventHandler(myport_DataReceived);
 
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error");
-            }
+            //try
+            //{
+            //    myport.Open();
+            //}
 
-            
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error");
+            //}
+
+            DAQ DAC = new DAQ();
+            ////double voltage = DAC.get_voltage();
+            ////textBox1.Text = voltage.ToString();
+
+            this.Hide();
+
+            DAC.Show();
+
         }
 
         void myport_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             input = myport.ReadLine();
-            //ControlLabDAC DAC = new ControlLabDAC();
-            //DAC
-
+            
             this.Invoke(new EventHandler(displaydata_event));
         }
 
         private void displaydata_event(object sender, EventArgs e)
         {
-            
+
             textBox1.Text = input;
- 
+
         }
     }
 }
